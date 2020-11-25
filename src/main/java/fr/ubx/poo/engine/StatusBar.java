10a66ade4b541b06f22c1f36067cf5001893fcb4
Alexaline Thomas <thomas.alexaline@etu.u-bordeaux.fr>
2020-11-25 10:33:28 +0100

    the status bar now show the keys the bomb and the bomb range

--- src/main/java/fr/ubx/poo/engine/StatusBar.java
+++ src/main/java/fr/ubx/poo/engine/StatusBar.java
@@ -82,9 +82,9 @@ public class StatusBar {
     public void update(Game game) {
         updateLevel(1);
         liveValue.setText(String.valueOf(game.getPlayer().getLives()));
-        rangeValue.setText("?");
-        bombsValue.setText("?");
-        keyValue.setText("?");
+        rangeValue.setText(String.valueOf(game.getPlayer().getBombsRange()));
+        bombsValue.setText(String.valueOf(game.getPlayer().getBombCapacity()));
+        keyValue.setText(String.valueOf(game.getPlayer().getKeys()));
     }
 
 }
